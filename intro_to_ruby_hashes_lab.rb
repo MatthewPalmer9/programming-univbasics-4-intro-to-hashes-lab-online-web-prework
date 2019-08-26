50728d1222a4049618de921a866ed78a3d19727e
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-08-26 06:10:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,9 +29,9 @@ def update_counting_hash(hash, key)
   
   def update_hash 
     if hash[key]
-      hash[key] = 1
-    else
       hash[key] + 1
+    else
+      hash[key] = 1
     end
   end
 end
